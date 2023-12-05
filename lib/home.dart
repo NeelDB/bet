f6919d36861a439dd8f7815b60eff79ca95ced31
NeelDB <neel.badracim@glartek.com>
2023-12-05 17:34:41 +0100

    Pagina inicial finalizada

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,114 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+class WelcomeText extends StatelessWidget {
+  const WelcomeText({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SizedBox(
+          width: 214,
+          height: 35,
+          child: Text(
+            'Welcome!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 34,
+              fontFamily: 'Didact Gothic',
+              fontWeight: FontWeight.w400,
+              height: 0,
+              letterSpacing: -0.30,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+}
+
+class BetterEatThatLogo extends StatelessWidget {
+  const BetterEatThatLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          Image.asset(
+              'assets/BetterEatThat.png'
+          ),
+        ],
+      );
+  }
+}
+
+class GoogleSignInButton extends StatelessWidget {
+  const GoogleSignInButton({Key? key, required this.onPressed}) : super(key: key);
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 326,
+        height: 60,
+        padding: const EdgeInsets.all(10),
+        decoration: ShapeDecoration(
+          color: const Color(0xFFF9F7FF),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          shadows: const [
+            BoxShadow(
+              color: Color(0x26452A7C),
+              blurRadius: 30,
+              offset: Offset(0, 30),
+              spreadRadius: 0,
+            )
+          ],
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 44,
+              height: 44,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/google.png"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            const SizedBox(
+              width: 218,
+              height: 25,
+              child: Text(
+                'Continue with Google',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontFamily: 'Didact Gothic',
+                  fontWeight: FontWeight.w400,
+                  height: 0,
+                  letterSpacing: -0.30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
 class _HomeState extends State<Home> {
 
   Future<void> signInWithGoogle() async {
@@ -36,7 +144,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+      body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment(0.00, -1.00),
@@ -44,95 +152,29 @@ class _HomeState extends State<Home> {
             colors: [Color(0xFF72FF80), Color(0xFFC5FFD5), Color(0xFFB6D3FD)],
           ),
         ),
-        child: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top:450.0),
-              child: GestureDetector(
-                onTap: () async {
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const WelcomeText(),
+            const BetterEatThatLogo(),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: GoogleSignInButton(
+                onPressed: () async {
                   //Google Sign in
                   await signInWithGoogle();
-                  if(mounted) {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage(),));
+                  if (mounted) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProfilePage()),
+                    );
                   }
                 },
-                child: Container(
-                    width: 326,
-                    height: 60,
-                    padding: const EdgeInsets.all(10),
-                    decoration: ShapeDecoration(
-                      color: const Color(0xFFF9F7FF),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      shadows: const [
-                        BoxShadow(
-                          color: Color(0x26452A7C),
-                          blurRadius: 30,
-                          offset: Offset(0, 30),
-                          spreadRadius: 0,
-                        )
-                      ],
-                    ),
-                    child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 44,
-                            height: 44,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage("assets/google.png"),
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          const SizedBox(
-                            width: 218,
-                            height: 25,
-                            child: Text(
-                              'Continue with Google',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontFamily: 'Didact Gothic',
-                                fontWeight: FontWeight.w400,
-                                height: 0,
-                                letterSpacing: -0.30,
-                              ),
-                            ),
-                          ),
-                      ],
-                    )
-                ),
               ),
-            )
-        ),
-      )
-    );
-  }
-}
-
-class BetterEatThatLogo extends StatelessWidget {
-  const BetterEatThatLogo({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 392,
-          height: 422,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/BetterEatThat.png"),
-              fit: BoxFit.fill,
             ),
-          ),
-        )
-      ],
+          ],
+        ),
+      ),
     );
   }
 }
